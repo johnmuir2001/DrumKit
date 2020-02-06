4ab7f91b40804616c8c54435d3fca945080f3b96
John Muir <johnmuir@Johns-MacBook-Pro.local>
2020-02-06 16:47:32 +0000

    final css drum kit

--- script.js
+++ script.js
@@ -84,3 +84,77 @@ document.addEventListener('keypress', (keyValue) => {
         tom.play();
     }
   })
+
+let count = 0
+
+main.addEventListener("click", () => {
+    count++
+if(count%2 == 0){
+    main.style.animation = "mainDrum 0.1s linear"
+    setTimeout(() => {
+        main.style.removeProperty('animation')
+    }, 100)
+    boom.pause()
+    boom.currentTime = 0
+    boom.play();
+} else{
+    main.style.animation = "mainDrum 0.1s linear"
+        setTimeout(() => {
+            main.style.removeProperty('animation')
+        }, 100)
+        kick.pause()
+        kick.currentTime = 0
+        kick.play();
+}
+})
+
+tomDrum.addEventListener("click", () => {
+    tomDrum.style.animation = "tomDrum 0.25s linear"
+        setTimeout(() => {
+            tomDrum.style.removeProperty('animation')
+        }, 250)
+        tom.pause()
+        tom.currentTime = 0
+        tom.play();
+})
+
+document.getElementsByClassName("hihatContainer")[0].addEventListener("click", () => {
+    count++
+if(count%5 == 0){
+    openhatCymbal.style.animation = "openHihat 0.1s linear"
+        setTimeout(() => {
+            openhatCymbal.style.removeProperty('animation')
+        }, 100)
+        openhat.pause()
+        openhat.currentTime = 0
+        openhat.play();
+} else{
+    hihatCymbal.style.animation = "hihatCymbal 0.1s linear"
+        setTimeout(() => {
+            hihatCymbal.style.removeProperty('animation')
+        }, 100)
+        hihat.pause()
+        hihat.currentTime = 0
+        hihat.play();
+}
+})
+
+document.getElementsByClassName("snareContainer")[0].addEventListener("click", () => {
+    snareDrum.style.animation = "snareDrum 0.1s linear"
+        setTimeout(() => {
+            snareDrum.style.removeProperty('animation')
+        }, 100)
+        snare.pause()
+        snare.currentTime = 0
+        snare.play();
+})
+
+document.getElementsByClassName("cymbalContainer")[0].addEventListener("click", () => {
+    mainCymbal.style.animation = "mainCymbal 0.1s linear"
+        setTimeout(() => {
+            mainCymbal.style.removeProperty('animation')
+        }, 100)
+        ride.pause()
+        ride.currentTime = 0
+        ride.play();
+})
